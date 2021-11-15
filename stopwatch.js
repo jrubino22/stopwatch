b21309a207dfca8858385d5f3244c1f802df1de5
jrubino22 <93841868+jrubino22@users.noreply.github.com>
2021-11-14 17:05:12 -0800

    Commit 2

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,15 @@
 let sec = 0;
 let min = 0;
 let hr = 0;
+const buttons = document.getElementsByClassName("button");
+let stopTimer = true;
 
-let stopTimer = true
+buttons.forEach(function (btn) {
+    btn.addEventListener("click", function (c) {
+        console.log(c.currentTarget.classList)
+    
+    const start = Date.now(),
+    stop = Date.now(),
+    diff = stop-start;
 
+})})
