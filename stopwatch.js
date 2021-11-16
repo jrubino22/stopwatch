effbd7ff5f83daf52a5e261b8fa1ada91b738538
jrubino22 <93841868+jrubino22@users.noreply.github.com>
2021-11-15 16:53:32 -0800

    Commit3

--- stopwatch.js
+++ stopwatch.js
@@ -1,15 +1,36 @@
 let sec = 0;
 let min = 0;
 let hr = 0;
-const buttons = document.getElementsByClassName("button");
+
+let time = 10000000;
+
+
+const buttons = document.querySelectorAll(".button");
 let stopTimer = true;
+
+function timeToString () {
+    let diffInHrs = time / 3600000;
+let hh = Math.floor(diffInHrs);
+
+let diffInMins = (diffInHrs - hh) * 60;
+let mm = Math.floor(diffInMins);
+
+let diffInSecs = (diffInMins - mm) * 60;
+let ss = Math.floor(diffInSecs)
+
+let formattedHH = hh.toString().padStart(2, "0");
+let formattedMM = mm.toString().padStart(2, "0");
+let formattedSS = ss.toString().padStart(2, "0");
+
+return `${formattedHH}:${formattedMM}:${formattedSS}`;
+}
+
+
 
 buttons.forEach(function (btn) {
     btn.addEventListener("click", function (c) {
-        console.log(c.currentTarget.classList)
-    
-    const start = Date.now(),
-    stop = Date.now(),
-    diff = stop-start;
+        const classes = c.currentTarget.classList;
+       
+    })
 
-})})
+})
